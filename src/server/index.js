f98d64ed430275c1753b608800a7c240247b373e
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-13 11:39:53 -0400

    Got the api working

--- src/server/index.js
+++ src/server/index.js
@@ -6,32 +6,36 @@ dotenv.config();
 
 // For the nlp (natural language processing)
 const aylien = require("aylien_textapi");
-// set aylien API credentias
-var textapi = new aylien({
-    application_id: "your-api-id",
-    application_key: "your-key"
-  });
-
 // set aylien API credentials
-var textapi = new aylien({
+var nlp = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
 });
 
+// Set up express and needed packaged
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+var bodyParser = require('body-parser')
+var cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
+// Initial directory to use
+// This is the default directory after webpack packs things up
+// Can be changed in the output in the config files if wanted
 app.use(express.static('dist'))
 
-console.log(__dirname)
+//console.log(__dirname)
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+// app.get('/', function (req, res) {
+//     res.sendFile('dist/index.html')
+//     //res.sendFile(path.resolve('src/client/views/index.html'))
+// })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
@@ -40,4 +44,18 @@ app.listen(8081, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.get('/api', function (req, res) {
+  console.log(nlp.summarize({
+    url: 'http://techcrunch.com/2015/04/06/john-oliver-just-changed-the-surveillance-reform-debate',
+    sentences_number: 3
+  }, function(error, response) {
+    if (error === null) {
+      response.sentences.forEach(function(s) {
+        console.log(s);
+      });
+    }
+  }));
+  res.send(nlp)
 })
